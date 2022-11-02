dd84b1f53b4443cccfff75314482ab3ccfc00960
DanielFliman <90574202+DanielFliman@users.noreply.github.com>
2022-11-02 16:29:03 -0400

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -5,17 +5,24 @@ public class Main {
 	public static double Calculate(double com) {
 		double pay = 0;
 		
+		//Pay if making less than $15,000 in spread, you are given 2% of that weekly spread as take home pay
 		if (com < 15000) {
 			pay = com *= .02;
-		} else if (com >= 15000 && com < 30000) {
+		} 
+		//Pay if making more than $15,000 in weekly spread but less than $30,000, you are given 6% of that weekly spread as take home pay
+		else if (com >= 15000 && com < 30000) {
 			com -= 15000;
 			pay = com *= .06;
 			pay += 300;
-		} else if (com >= 30000 && com < 45000) {
+		} 
+		//Pay if making more than $30,000 in weekly spread but less than $45,000, you are given 8% of that weekly spread as take home pay
+		else if (com >= 30000 && com < 45000) {
 			com -= 30000;
 			pay = com *= .08;
 			pay += 1200;
-		} else {
+		} 
+		//Pay if making more than $45,000 in spread, you are given 10% of that weekly spread as take home pay
+		else {
 			com -= 45000;
 			pay = com *= .1;
 			pay += 2400;
